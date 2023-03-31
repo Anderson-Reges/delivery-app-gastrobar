6ca2712e8732231259ca29ba615f507977655f8c
Anderson-Reges <andersonreges76@gmail.com>
2023-03-30 22:49:43 -0300

    feat: removendo consoles logs

--- back-end/src/api/controllers/Access.controller.js
+++ back-end/src/api/controllers/Access.controller.js
@@ -4,7 +4,7 @@ const LoginControl = async (req, res) => {
   const { email, password } = req.body;
 
   const result = await Login(email, password);
-  console.log(email, password);
+
   return res.status(200).json(result);
 };
 
