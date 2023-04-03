9f4e069677b8551c62b02c9f1405ba47930433df
Anderson-Reges <andersonreges76@gmail.com>
2023-04-03 19:10:03 -0300

    fix: docker-compose

--- back-end/src/api/services/Access.service.js
+++ back-end/src/api/services/Access.service.js
@@ -1,18 +1,18 @@
+const jwtKey = require('fs')
+  .readFileSync('jwt.evaluation.key', { encoding: 'utf-8' });
 const jwt = require('jsonwebtoken');
 const md5 = require('md5');
 const dotenv = require('dotenv');
 const { User } = require('../../database/models');
 
 dotenv.config(); // importa variavel de ambiente do .ENV
-
-const secret = process.env.JWT_SECRET || 'secret_key'; 
 // || 'secret'; é para caso não tenha sido definido a variavel ambiente
 const jwtConfig = { expiresIn: '1d', algorithm: 'HS256' };
 
 const Login = async (email, _password) => {
   const user = await User.findOne({ where: { email }, raw: true });
   const newUser = { name: user.name, email: user.email, role: user.role };
-  const token = jwt.sign(newUser, secret, jwtConfig);
+  const token = jwt.sign(newUser, jwtKey, jwtConfig);
 
   return {
     ...newUser,
@@ -27,7 +27,7 @@ const Register = async (newAccount) => {
     email: newAccount.email,
     role: newAccount.role || 'customer',
   };
-  const token = jwt.sign(newUser, secret, jwtConfig);
+  const token = jwt.sign(newUser, jwtKey, jwtConfig);
   const account = {
     name: newAccount.name,
     email: newAccount.email,
