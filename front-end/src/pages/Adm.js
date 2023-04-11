848abff86f0cadece2ba52848a581ed7a06a9163
Anderson-Reges <andersonreges76@gmail.com>
2023-04-11 02:12:18 -0300

    fix: lint

--- front-end/src/pages/Adm.js
+++ front-end/src/pages/Adm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import '../App.css';
 import MyContext from '../context/Context';
 import api from '../utils/fetch';
